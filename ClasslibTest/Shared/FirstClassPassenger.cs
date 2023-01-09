ef8a65323cab47db6d8902afe7c9d2b0ffbae95e
mostofa kamal <mostofakamal1990@gmail.com>
2023-01-09 07:17:37 +0600

    shared file

---
+++ ClasslibTest/Shared/FirstClassPassenger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasslibTest.Shared
+{
+    public class FirstClassPassenger : Passenger
+    {
+        public int AirMiles { get; set; }
+
+        public override string ToString()
+        {
+            return $"First Class with {AirMiles:N0} air miles: {Name}";
+        }
+    }
+}
