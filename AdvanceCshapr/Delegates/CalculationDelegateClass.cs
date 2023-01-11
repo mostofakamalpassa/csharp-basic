a4fae7b7480497fc4b4f82995bbaa2e72d0c287d
mkpbd <mostofakamal1990@gmail.com>
2023-01-11 11:14:33 +0600

    delegate

---
+++ AdvanceCshapr/Delegates/CalculationDelegateClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceCshapr.Delegates
+{
+    public delegate int Transformer(int x);
+    public class CalculationDelegateClass
+    {
+
+        public int sum (int x)
+        {
+            return x + x;
+        }
+
+        public int Multiply(int x)
+        {
+            return x * x;
+        }
+    }
+}
---
+++ AdvanceCshapr/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+using AdvanceCshapr.Delegates;
+
+CalculationDelegateClass delC = new CalculationDelegateClass();
+
+Transformer trans = delC.sum;
+Transformer tran = delC.Multiply;
+Console.WriteLine(trans.Invoke(6));
+Console.WriteLine(tran.Invoke(5));
